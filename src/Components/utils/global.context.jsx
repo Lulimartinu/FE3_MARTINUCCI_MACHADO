daa424f5da78e97b4fa31afb934085b9a2863ade
Lulimartinu <lulimartinucci@hotmail.com>
2023-12-01 17:33:44 -0300

    global

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,14 +1,45 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-export const initialState = {theme: "", data: []}
+export const initialState = {
+  theme: "light",
+  data: [],
+};
 
-export const ContextGlobal = createContext(undefined);
+export const ContextGlobal = createContext({});
+
+// global para el cambio de color, light o dark
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_THEME": {
+      const root = document.documentElement;
+      if (state.theme === "dark") {
+        root.classList.remove("light");
+      } else {
+        root.classList.add("dark");
+      }
+      const newTheme = state.theme === "light" ? "dark" : "light";
+      return { ...state, theme: newTheme };
+    }
+
+    default:
+      return state;
+  }
+};
 
 export const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
 
+  function changeTheme() {
+    dispatch({ type: "TOGGLE_THEME" });
+  }
+
+  const contextValue = {
+    ...state,
+    changeTheme,
+  };
   return (
-    <ContextGlobal.Provider value={{}}>
+    <ContextGlobal.Provider value={{ contextValue }}>
       {children}
     </ContextGlobal.Provider>
   );
