5dba11bd6488452bdf324ac3a54c7789153a4322
Lulimartinu <lulimartinucci@hotmail.com>
2023-12-02 10:26:18 -0300

    pasa los datos de favs por consola pero no añade las cards en la pantalla

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,26 +1,23 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ContextGlobal } from "../Components/utils/global.context";
-
+//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 const Detail = () => {
   const { state } = useContext(ContextGlobal);
   const { id } = useParams();
 
   const [dentist, setDentist] = useState();
   const [err, setErr] = useState();
-  const [loading, setLoading] = useState(false);
-
+ 
   async function getDentist() {
     setLoading(true);
     try {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
       const data = await response.json();
       setDentist(data);
     } catch (error) {
       setErr(error.message);
-    } finally {
-      setLoading(false);
-    }
+    } 
   }
 
   useEffect(() => {
@@ -29,13 +26,20 @@ const Detail = () => {
 
   return (
     <div>
+       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
+      {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
+
       {err && <p>Error al obtener los detalles del dentista: {err}</p>}
-      {loading && <p>Cargando...</p>}
       {dentist && (
         <article>
-          <h2>{dentist.name}</h2>
+            {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
+      {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
+          <h2>Detalles del dentista : </h2>
+          <h3> {dentist.name}</h3>
           <p>{dentist.username}</p>
           <p>id: {dentist.id}</p>
+          <p> tel : {dentist.phone}</p>
+          <p>website : {dentist.website}</p>
         </article>
       )}
     </div>
--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -3,17 +3,19 @@ import { ContextGlobal } from "../Components/utils/global.context";
 import { Link } from "react-router-dom";
 import Card from "../Components/Card";
 
+//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+
+//NO RENDERIZA EN FAVS, PASA POR CONSOLA LOS DATOS PERO NO LO MUESTRA EN LA PANTALLA 
 
 const Favs = () => {
   const { state } = useContext(ContextGlobal);
 
   console.log("Estado en Favs:", state);
-  // Usa el estado del contexto para obtener la lista de dentistas favoritos
+
   const favs = JSON.parse(localStorage.getItem("favs")) || [];
 
   console.log(localStorage.getItem("favs"))
 
-  // Mostrar un mensaje si no hay dentistas favoritos
   if (favs.length === 0) {
     return <h2>No tienes dentistas favoritos !</h2>;
   }
@@ -22,7 +24,8 @@ const Favs = () => {
     <>
       <h2>Dentistas Favoritos :</h2>
       <div className={"card-grid"}>
-        {/* Renderizar la lista de dentistas favoritos */}
+      {/* este componente debe consumir los destacados del localStorage */}
+        {/* Deberan renderizar una Card por cada uno de ellos */}
         {favs.map((dentist) => (
           <div key={dentist.id}>
             <Card dentist={dentist} />
